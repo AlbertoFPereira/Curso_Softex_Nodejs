c88ab871f4a277700d39cab6e5731f368f6d90e8
Alberto Pereira <albertopereira2h@gmail.com>
2025-08-14 20:11:12 -0300

    Create desafio_03.js
    first_commit

---
+++ Aula_02/desafio_03.js/desafio_03.js
@@ -0,0 +1,10 @@
+/*
+Escreva um programa dada uma temperatura em graus Celsius (c), 
+elaborar um programa para converter a temperatura para graus Farenheit
+(f).
+*/
+
+let celsius = 26;
+let farenheit = ((9/5)*celsius)+32;
+console.log("A temperatura em Celsius é "+ celsius + '°. Já em Farenheit é ' + farenheit.toFixed(1) +'°');
+
