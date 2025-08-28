f5f51f0b68238849cb8c3e72a385d13343d82b87
Alberto Pereira <albertopereira2h@gmail.com>
2025-08-28 22:23:32 +0000

    Responder o Formulário - JavaScript

---
+++ atividadeMoodle_2/app.js
@@ -0,0 +1,93 @@
+//1 - Qual o resultado exibido?
+
+/*let arr = [10, 20, 30];
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = arr[i] / 10;
+}
+console.log(arr); */
+
+// 2 - Qual o valor de saida ao final?
+
+/*let saida = "";
+let i = 1;
+while (i <= 5) {
+  if (i % 2 === 0) {
+    saida += i + "-";
+  }
+  i++;
+}
+console.log(saida); */
+
+
+// 3 - Qual será a saida do codigo abaixo?
+
+let x = 5;
+if (x % 2 === 0) {
+  console.log("Par");
+} else if (x % 3 === 0) {
+  console.log("Divisível por 3");
+} else {
+  console.log("Nenhum");
+
+
+// 4 - O que será exibido no console?
+
+/*let i = 5;
+do {
+  console.log(i);
+  i--;
+} while (i > 0 && i % 2 === 0); */
+
+
+// 5 - O que imprime?
+
+/*let matriz = [[1,2,3],[4,5,6]];
+console.log(matriz[0].length); */
+
+
+// 6 - O que imprime o código?
+
+/*let i = 0;
+do {
+  i++;
+} while (i < 5);
+console.log(i); */
+
+
+// 7 - Qual saída?
+
+/*let arr = [1,2,3]; 
+arr.pop(); 
+console.log(arr); */
+
+
+// 8 - Qual será a saída?
+
+/* let soma = 0;
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) continue;
+  soma += i;
+}
+console.log(soma); */
+
+
+// 9 - Qual será a saída do código abaixo?
+
+/*let arr = [2, 4, 6, 8];
+let soma = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 4 === 0) continue;
+  soma += arr[i];
+}
+console.log(soma); */
+
+
+
+// 10 - Qual a saída?
+
+/* let arr = [1,2,3]; 
+for (let valor of arr) { 
+  console.log(valor); 
+}
+  */
--- aula_07/função.js
+++ aula_07/função.js
@@ -14,21 +14,21 @@ saudar(nome);*/
 
 // Função Soma Arrow Function (=>) para a mesma atividade acima:
 
-/*const prompt = require('prompt-sync')({singint: true});
+const prompt = require('prompt-sync')({singint: true});
 
 let nome = prompt('Digite seu primeiro nome:');
 const saudar = nome => console.log("Olá, " + nome + "!");
 
-saudar(nome); */
+saudar(nome);
 
     
 //Crie uma função que recebe 2 números  e retorne a soma ou a divisão entre eles, dependendo da escolha do usuário
 
-const prompt = require('prompt-sync')({singint: true});
+/*nst prompt = require('prompt-sync')({singint: true});
 
 let 
 let numero1 = prompt('Digite o primeiro número: ');
 let numero2 = prompt('Digite o segundo número: ');
 
 const somar = (numero1 + numero2) 
-const divisao
+const divisao                         */
